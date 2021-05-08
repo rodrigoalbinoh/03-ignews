eccc1a421472938e12ef4b8c921ea73b00706d99
Rodrigo <rodrigo.hammes7@gmail.com>
2021-05-07 23:24:42 -0300

    título dinâmico para cada página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-bt">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          
-          <title>ig.news</title>
-          
+          <link rel="preconnect" href="https://fonts.gstatic.com" />          
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
