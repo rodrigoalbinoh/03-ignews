1864a1c2a82889638150bfdb552922d2d78ebf9a
Rodrigo A Hammes <rodrigo.hammes7@gmail.com>
2021-07-06 12:56:05 -0300

    exibe posts do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updated_at: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return(
     <>
       <Head>
@@ -9,55 +24,46 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
-
-          <a href="/">
-            <time>12 de março de 2030</time>
-            <strong>Um titulo bem grande</strong>
-            <p>Um parágrafo do texto bem criativo ou talvez não porque to com preguiça de entrar no lipsum.com</p>
-          </a>
+         { posts.map(post => (
+            <a key={post.slug} href={post.slug}>
+              <time>{post.updated_at}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+         )) }
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.test ?? '',
+      updated_at: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
